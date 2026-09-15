3c89d9cdbce62f47f604b32db91a656e9237feec
laserworksideas-coder <laserworksideas@gmail.com>
2026-09-15 12:39:12 +0300

    Create products.js

---
+++ products.js
@@ -0,0 +1,38 @@
+const products = [
+  {
+    name: "Personalised Wooden Gift",
+    description: "Handmade and personalised especially for you.",
+    price: "€15",
+    image: "images/product-1.jpg"
+  },
+  {
+    name: "Acrylic Personalised Gift",
+    description: "Beautiful acrylic creation with your own name or message.",
+    price: "€12",
+    image: "images/product-2.jpg"
+  },
+  {
+    name: "Baptism Creation",
+    description: "Personalised handmade details for a special baptism.",
+    price: "€10",
+    image: "images/product-3.jpg"
+  },
+  {
+    name: "Birthday Gift",
+    description: "Unique handmade gift for birthdays and special moments.",
+    price: "€10",
+    image: "images/product-4.jpg"
+  },
+  {
+    name: "School Gift",
+    description: "Cute personalised gifts for teachers and children.",
+    price: "€3",
+    image: "images/product-5.jpg"
+  },
+  {
+    name: "Christmas Creation",
+    description: "Handmade personalised Christmas decoration or gift.",
+    price: "€5",
+    image: "images/product-6.jpg"
+  }
+];

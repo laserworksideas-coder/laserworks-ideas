688a3c6fe56f6f8260508008b2bf303f3f59c89c
laserworksideas-coder <laserworksideas@gmail.com>
2026-09-21 13:13:48 +0300

    Add category photo lists

--- products.js
+++ products.js
@@ -45,4 +45,144 @@ const mainCategoryPhotos = {
   "CLOTHING & TOWELS": ""
 };
 
+
+const categoryPhotos = {
+  "BALLOONS, SOAPS & CANDY CAKES": [
+    "ballons (10).jpg",
+    "ballons (11).png",
+    "ballons (3).jpg",
+    "ballons (4).jpg",
+    "ballons (5).jpg",
+    "ballons (6).jpg",
+    "ballons (7).jpg",
+    "ballons (8).jpg",
+    "ballons (9).jpg",
+    "soaps (10).jpg",
+    "soaps (11).jpg",
+    "soaps (5).jpg",
+    "soaps (6).jpg",
+    "soaps (7).jpg",
+    "soaps (8).jpg",
+    "soaps (9).jpg"
+  ],
+  "BIRTHDAY & PARTIES": [
+    "BIRTHDAY (2).jpg",
+    "BIRTHDAY (3).jpg",
+    "BIRTHDAY (33).jpg",
+    "BIRTHDAY (36).jpg",
+    "BIRTHDAY (37).jpg",
+    "BIRTHDAY (38).jpg",
+    "BIRTHDAY (39).jpg",
+    "BIRTHDAY (5).jpg",
+    "BIRTHDAY (6).jpg",
+    "birthday (1).jpg",
+    "birthday (21).jpg",
+    "birthday (22).jpg",
+    "birthday (23).jpg",
+    "birthday (24).jpg",
+    "birthday (25).jpg",
+    "birthday (26).jpg",
+    "birthday (27).jpg",
+    "birthday (28).jpg",
+    "birthday (29).jpg",
+    "birthday (30).jpg",
+    "birthday (31).jpg",
+    "birthday (32).jpg",
+    "birthday (33).jpg",
+    "birthday (34).jpg",
+    "birthday (35).jpg",
+    "birthday (36).jpg",
+    "birthday (37).jpg",
+    "birthday (38).jpg",
+    "birthday (39).jpg",
+    "birthday (40).jpg",
+    "birthday (41).jpg",
+    "birthday (42).jpg",
+    "birthday (43).jpg",
+    "birthday (44).jpg",
+    "birthday.jpg"
+  ],
+  "CHRISTENING & NEWBORNS": [
+    "CHRISTENING (1).jpg",
+    "CHRISTENING (10).jpg",
+    "CHRISTENING (11).jpg",
+    "CHRISTENING (12).jpg",
+    "CHRISTENING (13).jpg",
+    "CHRISTENING (14).jpg",
+    "CHRISTENING (15).jpg",
+    "CHRISTENING (2).jpg",
+    "CHRISTENING (3).jpg",
+    "CHRISTENING (4).jpg",
+    "CHRISTENING (5).jpg",
+    "CHRISTENING (6).jpg",
+    "CHRISTENING (7).jpg",
+    "CHRISTENING (8).jpg",
+    "CHRISTENING (9).jpg",
+    "NEWBORNS (1).jpg",
+    "NEWBORNS (1).png",
+    "NEWBORNS (2).jpg",
+    "NEWBORNS (3).jpg",
+    "NEWBORNS (4).jpg",
+    "NEWBORNS (5).jpg",
+    "NEWBORNS (6).jpg",
+    "NEWBORNS (7).jpg",
+    "NEWBORNS (8).jpg",
+    "NEWBORNS (9).jpg"
+  ],
+  "CORPORATE GIFTS": [
+    "CORPORATE (10).jpg",
+    "CORPORATE (11).jpg",
+    "CORPORATE (12).jpg",
+    "CORPORATE (13).jpg",
+    "CORPORATE (5).jpg",
+    "CORPORATE (6).jpg"
+  ],
+  "INVITATIONS & DESIGN": [
+    "invitation.jpg"
+  ],
+  "LASER ENGRAVING": [
+    "engraving.jpg"
+  ],
+  "NAMES, MONOGRAMS & TOPPERS": [
+    "TOPPERS (1).jpg",
+    "TOPPERS (10).jpg",
+    "TOPPERS (11).jpg",
+    "TOPPERS (12).jpg",
+    "TOPPERS (13).jpg",
+    "TOPPERS (14).jpg",
+    "TOPPERS (15).jpg",
+    "TOPPERS (16).jpg",
+    "TOPPERS (17).jpg",
+    "TOPPERS (2).jpg",
+    "TOPPERS (3).jpg",
+    "TOPPERS (4).jpg",
+    "TOPPERS (6).jpg",
+    "TOPPERS (7).jpg",
+    "TOPPERS (8).jpg",
+    "TOPPERS (9).jpg"
+  ],
+  "SCHOOL & TEACHER GIFTS": [
+    "SCHOOL (11).jpg",
+    "SCHOOL (13).jpg",
+    "SCHOOL (5).jpg",
+    "SCHOOL (9).jpg",
+    "school (1).jpg",
+    "school (2).jpg",
+    "school (3).jpg"
+  ],
+  "SEASONAL": [
+    "seasonal.jpg"
+  ],
+  "WEDDING": [
+    "WEDDING (10).jpg",
+    "WEDDING (2).jpg",
+    "WEDDING (7).jpg",
+    "WEDDING (8).jpg",
+    "WEDDING (9).jpg",
+    "wedding (1).jpg",
+    "wedding (2).jpg",
+    "wedding (3).jpg"
+  ]
+};
+
 const products = [];
